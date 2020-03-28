93cff4a2773604141697eed884a65f55900db7aa
Peter Gomez <fadedlamp42@gmail.com>
2020-03-28 14:43:10 -0400

    menu.py can now accept arguments in format "menu.py file_1.txt file_2.txt". created example.txt input file

--- menu.py
+++ menu.py
@@ -2,28 +2,39 @@ import pandas as pd
 import sys, os
 from datetime import date, datetime
 
-file = os.path.expanduser("~/hw.txt")
-if os.stat(file).st_size != 0:
-    data = pd.read_csv(
-        file,
-        sep='|',
-        skip_blank_lines=True,
-        skipinitialspace=True,
-        parse_dates=[1])
+if len(sys.argv) == 1:
+    raw = [os.path.expanduser("~/hw.txt")] #default for convenient school use 
 else:
-    print("hw.txt is empty!")
-    sys.exit(0)
-
-datetime.today()
+    raw = sys.argv[1:]
 
 def days_until(due):
     return (due - datetime.today()).days + 1
 
-for _, r in data.iterrows():
-    task=r[0]
-    required = r[2]
-    due=datetime.strptime(r[1], "%A %m/%d ")
-    due = due.replace(year=date.today().year)
-    days = days_until(due)
-    if(days<=required):
-        print(f"{days} day{'' if days==1 else 's'} until {task}({required} needed)")
+for f in raw:
+    if os.path.isfile(f): #guard for file existence
+        if os.stat(f).st_size != 0: #check if file is non-empty
+            data = pd.read_csv(
+                f,
+                sep='|',
+                skip_blank_lines=True,
+                skipinitialspace=True,
+                parse_dates=[1],
+                names=['task', 'due date', 'required'])
+            skip = False
+        else:
+            print(f"{f} is empty!")
+            skip = True
+    else:
+        print(f"{f} isn't a file!")
+        skip = True
+
+    if not skip:
+        for _, r in data.iterrows():
+            task = r[0]
+            required = r[2]
+            due = datetime.strptime(r[1], "%A %m/%d ")
+            due = due.replace(year=date.today().year)
+            until = days_until(due)
+
+            if until<=required and until>0:
+                print(f"{until} day{'' if until==1 else 's'} until {task}({required} needed)")
